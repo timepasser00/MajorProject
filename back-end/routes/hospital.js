bdcccd6f85d56a34400fa14c1728a34dea8ff940
Faisal Rahman <istevefaisal@gmail.com>
2023-01-27 23:04:04 +0530

    added two apis for the hospital

--- back-end/routes/hospital.js
+++ back-end/routes/hospital.js
@@ -1,10 +1,11 @@
 const express=require("express");
-const {registerRequest,allRequests,getAllHospitals, registerDoctor}=require("../controller/hospitalController");
+const {registerRequest,allRequests,getAllHospitals, registerDoctor,removeDoctor}=require("../controller/hospitalController");
 
 const router=express.Router();
 
 router.post("/register",registerRequest)
 router.get("/requests",allRequests)
 router.get("/hospitals",getAllHospitals);
-router.post("/registerDoctor",registerDoctor);
+router.post("/registerDoctor/:id",registerDoctor);
+router.delete("/removeDoctor/:id",removeDoctor);
 module.exports=router;
