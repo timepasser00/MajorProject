be22199ef8846c2563afdbff953a0196cd36b6a1
timepasser00 <atul03022019@gmail.com>
2023-01-27 23:45:46 +0530

    added build to .gitignore

--- back-end/app.js
+++ back-end/app.js
@@ -1,37 +1,52 @@
-const express = require('express');
-const cors=require('cors');
-const bodyParser = require('body-parser');
-require('dotenv').config()
-const homeRouter = require('./routes/home');
-const patientRouter=require('./routes/patients');
-const mongoose = require('mongoose');
-const hospitalRouter=require('./routes/hospital'); 
-const adminRouter=require('./routes/admin');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const homeRouter = require("./routes/home");
+const patientRouter = require("./routes/patients");
+const mongoose = require("mongoose");
+const hospitalRouter = require("./routes/hospital");
+const adminRouter = require("./routes/admin");
+const Web3 = require("web3");
+const contract = require("@truffle/contract");
+const artifacts = require("../build/contracts/base.json");
+const CONTACT_ABI = require("./config");
+const CONTACT_ADDRESS = require("./config");
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const port = 3001
+const port = 3001;
 
-app.use('/home',homeRouter)
-app.use('/patient',patientRouter)
-app.use('/hospital',hospitalRouter)
-app.use('/admin',adminRouter)
+if (typeof web3 !== "undefined") {
+  var web3 = new Web3(web3.currentProvider);
+} else {
+  var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"));
+}
 
-mongoose.connect('mongodb://localhost:27017/major-project', { useNewUrlParser: true, useUnifiedTopology: true,})
+app.use("/home", homeRouter);
+app.use("/patient", patientRouter);
+app.use("/hospital", hospitalRouter);
+app.use("/admin", adminRouter);
+
+mongoose
+  .connect("mongodb://localhost:27017/major-project", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-    console.log('connected to database')
+    console.log("connected to database");
     // listen to port
     app.listen(4000, () => {
-      console.log('listening for requests on port')
-    })
+      console.log("listening for requests on port");
+    });
   })
   .catch((err) => {
-    console.log(err)
-  }) 
+    console.log(err);
+  });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
